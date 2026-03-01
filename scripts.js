dff7fe07978e585a4bcdea70acaf9fc5649cee44
Louis <louisdrimer123@gmail.com>
2026-03-01 14:42:06 +0000

    Add input new book functionality

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function addBookToLibrary(title, author, length) {
     const book = new Book(title, author, length);
     myLibrary.push(book);
 
-    const libraryTable = document.getElementById("libraryTable");
+    const libraryTable = document.getElementById("library-table");
     const newRow = document.createElement("tr");
 
     // Use loop to append values to table
@@ -23,5 +23,21 @@ function addBookToLibrary(title, author, length) {
 
     libraryTable.appendChild(newRow);
 }
+
+const newBookSubmitButton = document.getElementById("new-book-submit-button");
+
+newBookSubmitButton.addEventListener("click", function() {
+    const author = document.getElementById("author-input")
+    const title = document.getElementById("title-input")
+    const length = document.getElementById("length-input")
+
+    addBookToLibrary(author.value, title.value, length.value);
+
+    author.value = "";
+    title.value = "";
+    length.value = "";
+});
+
+// Take text from input box upon button press, and store in 3 different variables
 
 // event.preventDefault();
