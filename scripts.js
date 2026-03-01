cb10fe775d7f37f98e7d8b3c01d57cc32f0750ec
Louis <louisdrimer123@gmail.com>
2026-03-01 12:48:50 +0000

    Add table appending functionality without using loop

--- scripts.js
+++ scripts.js
@@ -10,4 +10,26 @@ function Book(title, author, length) {
 function addBookToLibrary(title, author, length) {
     const book = new Book(title, author, length);
     myLibrary.push(book);
+
+    // Logic for appending new book to table without using a loop
+    const libraryTable = document.getElementById("libraryTable");
+
+    const newRow = document.createElement("tr");
+
+    const titleCell = document.createElement("td");
+    const authorCell = document.createElement("td");
+    const lengthCell = document.createElement("td");
+    const IDCell = document.createElement("td");
+
+    titleCell.innerText = book.title;
+    authorCell.innerText = book.author;
+    lengthCell.innerText = book.length;
+    IDCell.innerText = book.id;
+
+    newRow.appendChild(titleCell);
+    newRow.appendChild(authorCell);
+    newRow.appendChild(lengthCell);
+    newRow.appendChild(IDCell);
+
+    libraryTable.appendChild(newRow);
 }
